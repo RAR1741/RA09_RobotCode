af4e4cad297ceb4498483c713c6af8c665ef4fb4
patrick <patrick@6c4f5b9c-5adb-dd11-8969-000347c480a5>
2009-01-29 01:46:52 +0000

    Added code to manage autonomous and start two color tracking identification.
    git-svn-id: svn://svn03.redalert1741.com/RA09_RobotCode/trunk@61 6c4f5b9c-5adb-dd11-8969-000347c480a5

--- SimpleTemplate/Autonomous.cpp
+++ SimpleTemplate/Autonomous.cpp
@@ -1,24 +1,213 @@
+#include "Target.h"
 #include "Autonomous.h"
 
 //Autonomous::Autonomous(PurpleMonkeys * Robot)
-Autonomous::Autonomous()
-{
-	
+Autonomous::Autonomous() {
+
 }
 
-Autonomous::~Autonomous()
-{
+Autonomous::~Autonomous() {
 }
 
-void Autonomous::Init()
-{
-	
+void Autonomous::Init() {
+
+	/* image data for tracking - override default parameters if needed */
+	/* recommend making PINK the first color because GREEN is more 
+	 * subsceptible to hue variations due to lighting type so may
+	 * result in false positives */
+	// PINK
+	sprintf(td1.name, "PINK");
+	td1.hue.minValue = 220;
+	td1.hue.maxValue = 255;
+	td1.saturation.minValue = 75;
+	td1.saturation.maxValue = 255;
+	td1.luminance.minValue = 85;
+	td1.luminance.maxValue = 255;
+	// GREEN
+	sprintf(td2.name, "GREEN");
+	td2.hue.minValue = 55;
+	td2.hue.maxValue = 125;
+	td2.saturation.minValue = 58;
+	td2.saturation.maxValue = 255;
+	td2.luminance.minValue = 92;
+	td2.luminance.maxValue = 255;
+
+	/* set up debug output: 
+	 * DEBUG_OFF, DEBUG_MOSTLY_OFF, DEBUG_SCREEN_ONLY, DEBUG_FILE_ONLY, DEBUG_SCREEN_AND_FILE 
+	 */
+	SetDebugFlag(DEBUG_SCREEN_ONLY);
+
+	/* start the CameraTask	 */
+	if (StartCameraTask(framesPerSecond, 0, k320x240, ROT_180) == -1) {
+		DPRINTF(LOG_ERROR,
+				"Failed to spawn camera task; exiting. Error code %s",
+				GetVisionErrorText(GetLastVisionError()) );
+	}
+	/* allow writing to vxWorks target */
+	Priv_SetWriteFileAllowed(1);
 }
 
-void Autonomous::Continuous()
-{
-	
+void Autonomous::Continuous() {
+
 }
 
-void Autonomous::Periodic()
-{}
+
+/**
+	 * Set servo positions (0.0 to 1.0) translated from normalized values (-1.0 to 1.0). 
+	 * 
+	 * @param normalizedHorizontal Pan Position from -1.0 to 1.0.
+	 * @param normalizedVertical Tilt Position from -1.0 to 1.0.
+	 */
+	void setServoPositions(float normalizedHorizontal, float normalizedVertical)	{
+
+		float servoH = NormalizeToRange(normalizedHorizontal);
+		float servoV = NormalizeToRange(normalizedVertical);
+		
+		float currentH = horizontalServo->Get();		
+		float currentV = verticalServo->Get();
+		
+		/* make sure the movement isn't too small */
+		if ( fabs(servoH - currentH) > servoDeadband ) {
+			horizontalServo->Set( servoH );
+			/* save new normalized horizontal position */
+			horizontalPosition = RangeToNormalized(servoH, 1);
+		}
+		if ( fabs(servoV - currentV) > servoDeadband ) {
+			verticalServo->Set( servoV );
+			verticalPosition = RangeToNormalized(servoV, 1);
+		}
+	}	
+
+	/**
+	 * Adjust servo positions (0.0 to 1.0) translated from normalized values (-1.0 to 1.0). 
+	 * 
+	 * @param normalizedHorizontal Pan adjustment from -1.0 to 1.0.
+	 * @param normalizedVertical Tilt adjustment from -1.0 to 1.0.
+	 */
+	void adjustServoPositions(float normDeltaHorizontal, float normDeltaVertical)	{
+						
+		/* adjust for the fact that servo overshoots based on image input */
+		normDeltaHorizontal /= 8.0;
+		normDeltaVertical /= 4.0;
+		
+		/* compute horizontal goal */
+		float currentH = horizontalServo->Get();
+		float normCurrentH = RangeToNormalized(currentH, 1);
+		float normDestH = normCurrentH + normDeltaHorizontal;	
+		/* narrow range keep servo from going too far */
+		if (normDestH > 1.0) normDestH = 1.0;
+		if (normDestH < -1.0) normDestH = -1.0;			
+		/* convert input to servo range */
+		float servoH = NormalizeToRange(normDestH);
+
+		/* compute vertical goal */
+		float currentV = verticalServo->Get();
+		float normCurrentV = RangeToNormalized(currentV, 1);
+		float normDestV = normCurrentV + normDeltaVertical;	
+		if (normDestV > 1.0) normDestV = 1.0;
+		if (normDestV < -1.0) normDestV = -1.0;
+		/* convert input to servo range */
+		float servoV = NormalizeToRange(normDestV, 0.2, 0.8);
+
+		/* make sure the movement isn't too small */
+		if ( fabs(currentH-servoH) > servoDeadband ) {
+			horizontalServo->Set( servoH );		
+			/* save new normalized horizontal position */
+			horizontalPosition = RangeToNormalized(servoH, 1);
+		}			
+		if ( fabs(currentV-servoV) > servoDeadband ) {
+			verticalServo->Set( servoV );
+			verticalPosition = RangeToNormalized(servoV, 1);
+		}
+	}
+void Autonomous::Periodic() {
+	if (FindTwoColors(td1, td2, ABOVE, &par) ) {
+		//PrintReport(&par);
+		foundColor = true;
+		// reset pan		
+		panIncrement = 0;
+		if (par.imageTimestamp == savedImageTimestamp) {
+			// This image has been processed already, 
+			// so don't do anything for this loop 
+			staleImage = true;
+			DPRINTF(LOG_DEBUG, "STALE IMAGE");
+
+		} else {
+			// The target was recognized
+			// save the timestamp
+			staleImage = false;
+			savedImageTimestamp = par.imageTimestamp;
+			DPRINTF(LOG_DEBUG, "image timetamp: %lf", savedImageTimestamp);
+
+			// Here is where your game-specific code goes
+			// when you recognize the target
+
+			// get center of target 
+
+			// TODO: use par.average_mass_x_normalized and
+			// par.average_mass_y_normalized after updated WPILib is distributed
+			horizontalDestination = par.center_mass_x_normalized;
+			verticalDestination = par.center_mass_y_normalized;
+		}
+	} else { // need to pan 
+		foundColor = false;
+	}
+	if (foundColor && !staleImage) {
+		/* Move the servo a bit each loop toward the destination.
+		 * Alternative ways to task servos are to move immediately vs.
+		 * incrementally toward the final destination. Incremental method
+		 * reduces the need for calibration of the servo movement while
+		 * moving toward the target.
+		 */
+		incrementH = horizontalDestination - horizontalPosition;
+		// you may need to reverse this based on your vertical servo installation
+		//incrementV = verticalPosition - verticalDestination;
+		incrementV = verticalDestination - verticalPosition;
+		adjustServoPositions(incrementH, incrementV);
+
+		ShowActivity("** %s & %s found: Servo: x: %f  y: %f ** ", td1.name,
+				td2.name, horizontalDestination, verticalDestination);
+
+	} else { //if (!staleImage) {  // new image, but didn't find two colors
+
+		// adjust sine wave for panning based on last movement direction
+		if (horizontalDestination > 0.0) {
+			sinStart = PI/2.0;
+		} else {
+			sinStart = -PI/2.0;
+		}
+
+		/* pan to find color after a short wait to settle servos
+		 * panning must start directly after panInit or timing will be off */
+		if (panIncrement == 3) {
+			panInit(8.0); // number of seconds for a pan
+		} else if (panIncrement > 3) {
+			panForTarget(horizontalServo, sinStart);
+
+			/* Vertical action: In case the vertical servo is pointed off center,
+			 * center the vertical after several loops searching */
+			if (panIncrement == 20) {
+				verticalServo->Set( 0.5);
+			}
+		}
+		panIncrement++;
+
+		ShowActivity(
+				"** %s and %s not found                                    ",
+				td1.name, td2.name);
+	} // end if found color
+
+	// sleep to keep loop at constant rate
+	// this helps keep pan consistant
+	// elapsed time can vary significantly due to debug printout
+	currentTime = GetTime();
+	lastTime = currentTime;
+	if (loopTime > ElapsedTime(lastTime) ) {
+		Wait(loopTime - ElapsedTime(lastTime) ); // seconds
+	}
+}
+
+void Autonomous::IdentifyFriendOrFoe() {
+	search;
+	FindColor()
+}
--- SimpleTemplate/Autonomous.h
+++ SimpleTemplate/Autonomous.h
@@ -13,7 +13,9 @@ public:
 	void Init();
 	void Periodic();
 	void Continuous();
+	void IdentifyFriendOrFoe();
 private:
-	
+	TrackingThreshold td1, td2;		// color thresholds			
+	ParticleAnalysisReport par;		// particle analysis report
 };
 #endif
