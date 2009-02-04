29f559134193f289acd722563bbb8c74cfcfd139
Jay <Jay@6c4f5b9c-5adb-dd11-8969-000347c480a5>
2009-02-04 00:03:37 +0000

    Conflict Resolved
    git-svn-id: svn://svn03.redalert1741.com/RA09_RobotCode/trunk@77 6c4f5b9c-5adb-dd11-8969-000347c480a5

--- SimpleTemplate/Personality.h
+++ SimpleTemplate/Personality.h
@@ -14,6 +14,7 @@ private:
 	SerialPort * terminal_port;
 	char SubMenuSelector;  // This state variable keeps track of sub menu from previous command
 	char TempSelector;     // Hash register for selection process
+	char Buffer[128];		// Buffer
 	char GetKeyPress (void);
 	void DisplayText (const char *TxtAdd);
 };
