caf2472ddbdf160160ee287832958be3e9729f45
Jay <Jay@6c4f5b9c-5adb-dd11-8969-000347c480a5>
2009-02-14 01:29:07 +0000

    git-svn-id: svn://svn03.redalert1741.com/RA09_RobotCode/trunk@110 6c4f5b9c-5adb-dd11-8969-000347c480a5

--- SimpleTemplate/Robot.cpp
+++ SimpleTemplate/Robot.cpp
@@ -11,11 +11,6 @@
 using std::cout;
 using std::endl;
 using ::floor;
-// #include "vxWorks.h"
-// #include "AxisCamera.h"
-// #include "BaeUtilities.h"
-// #include "FrcError.h"
-// #include "PCVideoServer.h"
 #include "Autonomous.h"
 #include "Logger.h"
 
@@ -43,8 +38,6 @@ BOOL ToggleSwitch (BOOL Input, unsigned int *StateVariable);  // Proto for toggl
 
 class PurpleMonkeys : public IterativeRobot {
 	RobotDrive myRobot; // robot drive system
-	//Joystick stick; // only joystick
-	//Joystick test;
 	Joystick leftStick;
 	Joystick rightStick;
 	Joystick turretStick;
@@ -53,7 +46,6 @@ class PurpleMonkeys : public IterativeRobot {
  	// Logger logger;
     CameraControl theCamera;
 	DashboardDataFormat dashboardDataFormat;
-	// Encoder testEncoder;
 	Encoder LMQuadEncoder;
 	Encoder RMQuadEncoder;
 	Encoder LMWheelEncoder;
@@ -62,41 +54,24 @@ class PurpleMonkeys : public IterativeRobot {
 	Encoder ElevatorEncoder;
 	Encoder TurretEncoder;
 	
-	//Victor testMotor;
-	//Servo horizontalServo;
-	//Servo verticalServo;
 	int counter;
 	Gyro * testGyro;
 	Gyro * testTemp;
-	// bool printed;
-	// Autonomous Auto;
 	Personality * Squeeky;
 	Turret TheTurret;
 	AnalogChannel leftCurrent;
 	AnalogChannel rightCurrent;
-	//AnalogChannel elevatorCurrent;
 	AnalogChannel launchWheelsCurrent;
 	AnalogChannel turretPositionCurrent;
-	//AnalogChannel harvesterCurrent;
 	
 	Accelerometer testAccel_X,testAccel_Y,testAccel_Z;
-	//Jaguar Harvester_Motor;
-	// Jaguar Launch_Wheels_Motor;
-	// Jaguar Turret_Pos_Motor; // Outsourced to Turret.h/cpp by PJF at 20:28 02/09/2009
-	//Jaguar Elevator_Motor;
 	Solenoid Gate;
-	//Toggle HarvesterToggle;
-	//Toggle EjecterToggle;
 	RobotHarvester Harvester;
-	// RobotElevator Elevator;
 	// Solenoid Collector;
 	Toggle Trigger;
 	Launcher launcher;
 	// State Variables for toggle code.
 	
-	//// Harvester State Vars
-	//unsigned int UserRequestHarvesterEjectIdle;
-	//unsigned int UserRequestHarvesterRunStop;
 	// Elevator state vars
 	unsigned int UserRequestElevatorArmDisarm;
 	unsigned int UserRequestElevatorRunStop;
@@ -109,15 +84,12 @@ class PurpleMonkeys : public IterativeRobot {
 public:
 	PurpleMonkeys(void) :
 		myRobot(4, 5), // these must be initialized in the same order
-				//stick(1), // as they are declared above.
-				//test(2),
 				leftStick(1),
 				rightStick(2),
 				turretStick(3),
 				testMotorStick(4),
 				
 				light(4, 14),
-				// logger(),
 		  		theCamera(),
 				dashboardDataFormat(),
 				LMQuadEncoder(4,1,4,2,false),
@@ -128,37 +100,20 @@ public:
 				ElevatorEncoder(4,11,4,12,false),
 				TurretEncoder(4,13,4,14,false),
 				
-				// testMotor(4,3),
-				//horizontalServo(4,4),
-				//verticalServo(4,5),
-				
 				counter(0),
 				testGyro(NULL),
 				testTemp (NULL),
-				//printed(false)
-				
-				//Auto()
 				Squeeky(NULL),
 				TheTurret(),
 				leftCurrent(2,4),
 				rightCurrent(2,5),
-				//elevatorCurrent(2,1),
 				launchWheelsCurrent(2,2),
 				turretPositionCurrent(2,3),
-				//harvesterCurrent(2,6),
 				testAccel_X(1,3),
 				testAccel_Y(1,4),
 				testAccel_Z(1,5),
-				//Harvester_Motor(4, 6),
-				// Launch_Wheels_Motor(4,2),
-//				Turret_Pos_Motor(4,3),
-				//Elevator_Motor(4,1),
 				Gate(8,1),
-				//HarvesterToggle(&rightStick, 2),
-				//EjecterToggle(&leftStick, 2),
-				//RobotHarvester(UINT32 MotorSlot, UINT32 MotorChannel, UINT32 CurrentSlot, UINT32 CurrentChannel);
 				Harvester(4, 6, 2, 6),
-				// Elevator(4, 1, 2, 1),
 				// Collector(8,1),
 				Trigger(&leftStick, 2),
 				launcher(4,2)
@@ -166,166 +121,6 @@ public:
 		// GetTrackingData(YELLOW, PASSIVE_LIGHT);
 		
 	}
-
-	// StartCompetition loop
-	//void StartCompetition(void)
-	//{
-	//}
-
-//	void InitHarvester()
-//	{
-//		// HarvesterState = HARV_STATE_NOT_FULL;
-//		// HarvesterAutoMode = HARV_AUTO_MODE_COLLECT;
-//		
-//		if(!Gate.Get()) // Is the gate open?
-//			Gate.Set(false); // Yes? Then close it.
-//		
-//		// ElevatorState = ELEVATOR_STATE_FULL;
-//		// EdgeTrigger;
-//		Harvester_Motor.Set(0.0);
-//		
-//		//// Zero all state vars (prevents garbage bit's from getting in the way.)
-//		//UserRequestHarvesterEjectIdle =
-//		//UserRequestHarvesterRunStop = 0;
-//		//Harvester_Motor
-//	}
-	
-//	int ProcessHarvester(bool LoadElevator)
-//	{
-//		/*
-//		if(LoadElevator)
-//			HarvesterAutoMode = HARV_AUTO_MODE_LOAD;
-//		if(EjectToggle)
-//			UserRequestEjectIdle = !UserRequestEjectIdle;
-//		*/
-//		/*
-//		if (true) { // manual mode
-//			if (RunStopToggle && !LastButtonValue) {
-//				UserRequestRunStop = !UserRequestRunStop;
-//			} else {
-//				// Maintain state
-//			}
-//			if (EjectToggle && !LastEjectValue) {
-//				UserRequestEjectIdle = !UserRequestEjectIdle;
-//			}
-//
-//			int direction = (UserRequestEjectIdle ? 1 : -1);
-//			if (UserRequestRunStop) {
-//				Harvester_Motor.Set(.5 * direction);
-//			} else {
-//				Harvester_Motor.Set(0.0);
-//			}
-//		}
-//		
-//		LastButtonValue = RunStopToggle;
-//		LastEjectValue = EjectToggle;
-//		
-//		*/
-//		/*
-//		if (RunStopToggle) {
-//			Harvester_Motor.Set(.5);
-//		} else if (EjectToggle) {
-//			Harvester_Motor.Set(-.5);
-//		}
-//		*/
-//		
-//		HarvesterToggle.UpdateState();
-//		EjecterToggle.UpdateState();
-//
-//		if(1){// Put Manual/Auto if condition here. 
-//			// Manual Mode
-//			if (HarvesterToggle.GetOutput()){
-//				Harvester_Motor.Set(.5);
-//				EjecterToggle.Reset();
-//			}
-//			else{
-//				if(EjecterToggle.GetOutput()){
-//					Harvester_Motor.Set(-.5);
-//				}
-//				else{
-//					Harvester_Motor.Set(0.0);
-//				}
-//			}
-//		}
-//		else{
-//			// Auto mode code should be here.
-//		}
-//		
-//		return 0;
-//	}
-	
-//	int ProcessHarvester(BOOL RunStopToggle, BOOL EjectToggle, bool LoadElevator)
-//	{
-//		/*
-//		if(LoadElevator)
-//			HarvesterAutoMode = HARV_AUTO_MODE_LOAD;
-//		if(EjectToggle)
-//			UserRequestEjectIdle = !UserRequestEjectIdle;
-//		*/
-//		/*
-//		if (true) { // manual mode
-//			if (RunStopToggle && !LastButtonValue) {
-//				UserRequestRunStop = !UserRequestRunStop;
-//			} else {
-//				// Maintain state
-//			}
-//			if (EjectToggle && !LastEjectValue) {
-//				UserRequestEjectIdle = !UserRequestEjectIdle;
-//			}
-//
-//			int direction = (UserRequestEjectIdle ? 1 : -1);
-//			if (UserRequestRunStop) {
-//				Harvester_Motor.Set(.5 * direction);
-//			} else {
-//				Harvester_Motor.Set(0.0);
-//			}
-//		}
-//		
-//		LastButtonValue = RunStopToggle;
-//		LastEjectValue = EjectToggle;
-//		
-//		*/
-//		/*
-//		if (RunStopToggle) {
-//			Harvester_Motor.Set(.5);
-//		} else if (EjectToggle) {
-//			Harvester_Motor.Set(-.5);
-//		}
-//		*/
-//		
-//		BOOL RSToggle = ToggleSwitch(RunStopToggle, &UserRequestHarvesterRunStop);
-//		BOOL EJToggle;
-//		
-//		// Only toggle eject if harvester is off.
-//		if(RSToggle==false)
-//			EJToggle = ToggleSwitch(EjectToggle, &UserRequestHarvesterEjectIdle);
-//		if(1){// Put Manual/Auto if condition here. 
-//			// Manual Mode
-//			if(RSToggle){
-//				Harvester_Motor.Set(.5);
-//			}
-//			else{
-//				if(EJToggle){
-//					Harvester_Motor.Set(-.5);
-//				}
-//				else{
-//					Harvester_Motor.Set(0.0);
-//				}
-//			}
-//		}
-//		else{
-//			// Auto mode code should be here.
-//		}
-//		
-//		// LastButtonValue = RunStopToggle;
-//
-//		return 0;
-//	}
-	
-//	void InitElevator()
-//	{
-//		
-//	}
 
 	// One time initialization of the robot
 	void RobotInit(void) {
@@ -346,8 +141,6 @@ public:
 		 }
 		   dprintf("Robot initialized.");
 		   
-		  //InitHarvester();
-			
 		// This should probably be in the Elevator code
 		if(!Gate.Get()) // Is the gate open?
 			Gate.Set(false); // Yes? Then close it.
@@ -385,8 +178,6 @@ public:
 	void AutonomousInit(void) {
 		Squeeky->Close();
 		LMQuadEncoder.Stop();
-		//testMotor.Set(0.0);
-		//myRobot.Drive(0.5, 0.0); // drive forwards half speed
 		GetWatchdog().SetEnabled(false);
 		 // logger.OpenFile("log.log");
 		 // logger.Debug("Entering teleop mode...");
@@ -475,41 +266,13 @@ public:
 				testGyro = new Gyro(1,1);
 			}
 		
-		/*
-		bool RunStop,  EjectToggle;
-
-		if(leftStick.GetButton(leftStick.kTopButton))
-			EjectToggle = true;
-		else
-			EjectToggle = false;
-		
-		if(rightStick.GetButton(rightStick.kTopButton))
-			RunStop = true;
-		else
-			RunStop = false;
-		*/
-		
 		// Elevator.Process();
 		Harvester.Process(false);
 		launcher.Update();
-		//ProcessHarvester(false);
-		//ProcessHarvester(RunStop, EjectToggle, false);
 		
-		//
-		// 
-		//
 		
 //#warning "THIS IS TEST CODE. DON'T SEND THE ROBOT INTO COMPETITION"
 		
-		/*
-		
-		// Harvester for testing is run off of top button.
-		if (turretStick.GetButton(turretStick.kTopButton)) {
-			this->Harvester_Motor.Set(1.0);
-		} else {
-			this->Harvester_Motor.Set(0.0);
-		}
-		*/
 		// Map X-Axis of joystick to turret position
 		//Turret_Pos_Motor.Set(turretStick.GetX());
 		TheTurret.TurretControl(&turretStick);
@@ -597,73 +360,5 @@ public:
 
 START_ROBOT_CLASS(PurpleMonkeys);
 #endif
-
-
-//int ToggleSwitch_LED1_State = 0;  // Example state variable for ToggleSwitch
-//LED1_IO = ToggleSwitch(!BUTTON1_IO, &ToggleSwitch_LED1_State);  // Example use
-
-
-
-//  ***** ToggleSwitch *****
-/*  Use this function to convert a signal into a toggle function.
-    Call this with an input and the output will toggle on each cycle
-    of the leading edge of the input signal. You must maintain a state
-    variable that will persist during use of this function. It requires
-    and uses the low two bits. Pass this function the input signal
-    variable and an address to an integer to use for the state variable.
-    This function will return the state of the toggled output.
-	By Hugh Meyer February 11, 2009
-*/
-
-// NOTE: IMPORTANT!!!
-// I didn't see why HAM passed it an int pointer at first, but now I know
-// it needs to be that way, to save the state outside the function.
-//BOOL ToggleSwitch (BOOL Input, unsigned int *State)
-//{
-//  unsigned int s = *State;      // Dereference saved state to local variable
-//  s = s << 1 | Input;  // Shift it left and add the input bit to it
-//
-//  switch (s) {         // Use it as a state variable
-//
-//    case 0:        // Idle State Off
-//      *State = 0;  // Set NextState(0) - Spin here til the input goes to 1
-//      return 0;    // Input = 0, Output = 0
-//
-//    case 1:        // Leading Edge Detected - Input transitioned from 0 to 1
-//      *State = 1;  // Set NextState(3)
-//      return 1;    // Input = 1, Output = 1
-//
-//    case 2:        // Trailing Edge Detected - Input transitioned from 1 to 0
-//      *State = 3;  // Set NextState(6) - Return to Idle Stte On
-//      return 1;    // Input = 0, Output = 1
-//
-//    case 3:        // Waiting for release
-//      *State = 1;  // Set NextState(3) - Spin here until input goes to 0
-//      return 1;    // Input = 1, Output = 1
-//
-//    case 4:        // Trailing Edge Detected - Input transitioned from 1 to 0
-//      *State = 0;  // Set NextState(0) - Return to Idle State Off
-//      return 0;    // Input = 0, Output = 0
-//
-//    case 5:        // Waiting for release
-//      *State = 2;  // Set NextState(4) - Spin here until input goes to 0
-//      return 0;    // Input = 1, Output = 0
-//
-//    case 6:        // Idle State On
-//      *State = 3;  // Set NextState(6) - Spin here til the input goes to 1
-//      return 1;    // Input = 1, Output = 1
-//
-//    case 7:        // Leading Edge Detected - Input transitioned from 0 to 1
-//      *State = 2;  // Set NextState(5)
-//      return 0;    // Input = 1, Output = 0
-//
-//    default:
-//      return 0;
-//  }    
-//
-//}	
-//  ***** End of ToggleSwitch *****
-
-
 
 
